c57dc7b287a9e07acafce6e910ff1fa2f768e5b3
Kumar4967 <raktvoeimami@gmail.com>
2026-05-17 08:15:16 +0300

    Add user login page with db

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Advertisement> Ads { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
 }
 
 public class Product
@@ -18,4 +21,13 @@ public class Advertisement
     public int Id { get; set; }
     public int ProductId { get; set; }
     public Product? Product { get; set; }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    public string Username { get; set; } = null!;
+    [Required]
+    public string PasswordHash { get; set; } = null!;
 }
